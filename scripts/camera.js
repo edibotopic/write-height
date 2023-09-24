78fa517f4805a0956b3f42cec248d9b699efe1e4
edibotopic <edibotopic@gmail.com>
2023-09-24 14:17:50 +0100

    move camera to script

---
+++ scripts/camera.js
@@ -0,0 +1,21 @@
+let cameraCreate = (canvas, scene) => {
+  let cameraArc = new BABYLON.ArcRotateCamera(
+    'CameraArc',
+    0,
+    0,
+    10,
+    new BABYLON.Vector3(0, 0, 0),
+    scene
+  )
+
+  cameraArc.setPosition(new BABYLON.Vector3(-20.0, 0.7, 40))
+  cameraArc.attachControl(canvas, false)
+  cameraArc.lowerBetaLimit = 0.5
+  cameraArc.upperBetaLimit = 1.0
+  cameraArc.lowerRadiusLimit = 15
+  cameraArc.upperRadiusLimit = 40
+  // cameraArc.inertia = 0;
+  // cameraArc.panningInertia = 0;
+
+  return cameraArc
+}
--- scripts/main.js
+++ scripts/main.js
@@ -6,23 +6,7 @@ let meshify = () => {
     let scene = new BABYLON.Scene(engine)
     scene.clearColor = BABYLON.Color3.Black()
 
-    let cameraArc = new BABYLON.ArcRotateCamera(
-      'CameraArc',
-      0,
-      0,
-      10,
-      new BABYLON.Vector3(0, 0, 0),
-      scene
-    )
-
-    cameraArc.setPosition(new BABYLON.Vector3(-20.0, 0.7, 40))
-    cameraArc.attachControl(canvas, false)
-    cameraArc.lowerBetaLimit = 0.5
-    cameraArc.upperBetaLimit = 1.0
-    cameraArc.lowerRadiusLimit = 15
-    cameraArc.upperRadiusLimit = 40
-    // cameraArc.inertia = 0;
-    // cameraArc.panningInertia = 0;
+    cameraCreate(canvas, scene)
 
     let lightMain = new BABYLON.HemisphericLight(
       'lightMain',
