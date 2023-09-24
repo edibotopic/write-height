c9c4b32e2e2aac5913c40f51aa6dbc7f9a42803d
edibotopic <edibotopic@gmail.com>
2023-09-24 14:20:26 +0100

    move lights to script

--- scripts/main.js
+++ scripts/main.js
@@ -8,29 +8,7 @@ let meshify = () => {
 
     cameraCreate(canvas, scene)
 
-    let lightMain = new BABYLON.HemisphericLight(
-      'lightMain',
-      new BABYLON.Vector3(0.8, 0.1, 0.5),
-      scene
-    )
-    lightMain.diffuse = new BABYLON.Color3(1, 0.9, 0.9)
-    lightMain.intensity = 0.8
-
-    let lightPink = new BABYLON.DirectionalLight(
-      'lightPink',
-      new BABYLON.Vector3(0.0, -1, 0),
-      scene
-    )
-    lightPink.specular = new BABYLON.Color3(0.4, 0.0, 0.6)
-    lightPink.intensity = 0.2
-
-    let lightBlue = new BABYLON.DirectionalLight(
-      'lightBlue',
-      new BABYLON.Vector3(0.5, -0.2, 0),
-      scene
-    )
-    lightBlue.specular = new BABYLON.Color3(0.2, 0.1, 0.8)
-    lightBlue.intensity = 0.2
+    lightsCreate(scene)
 
     let image = document.getElementById('output')
     let ground = BABYLON.MeshBuilder.CreatePlane(
